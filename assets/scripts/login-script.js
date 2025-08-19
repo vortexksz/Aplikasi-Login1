b700d9e6d4aecf0c56f240f46cc2e698553b1522
Kevin Raydafa Al Gibran <kevinroy201104@gmail.com>
2025-08-19 21:28:17 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,22 +1,21 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/*Membuat variabel untuk setiap element view */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/*Mmebuat variabel untuk menyimpan informasi email dan password */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/*Menambahkan Aksi klik pada button */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /*mendapatkan nilai dari masing masing input */
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+
+  /*memastikan nilai email dan password sesuai dengan nilai yang tersimpan */
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
   } else {
